5bc28ace684acf476b270d8ec94556f59ab0bd9d
wesley matlock <wesley.matlock@mlb.com>
2021-01-26 07:41:55 -0700

    Fruit Data model hooked up to onboarding screen.

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct FruitCardView: View {
 
   // MARK: - Properties
+  var fruit: Fruit
 
   @State private var isAnimating: Bool = false
 
@@ -11,21 +12,21 @@ struct FruitCardView: View {
     ZStack {
       VStack(spacing: 20) {
         // Fruit: Image
-        Image("blueberry")
+        Image(fruit.image)
           .resizable()
           .scaledToFit()
           .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8 )
           .scaleEffect(isAnimating ? 1.0 : 0.6)
 
         // Fruit: Title
-        Text("Blueberry")
+        Text(fruit.title)
           .foregroundColor(.white)
           .font(.largeTitle)
           .fontWeight(.heavy)
           .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2 )
 
         // Fruit: Headline
-        Text("Blueberries are sweet, nutritious and widly popular fruit all over the world")
+        Text(fruit.headline)
           .foregroundColor(.white)
           .multilineTextAlignment(.center)
           .padding(.horizontal, 16)
@@ -41,7 +42,7 @@ struct FruitCardView: View {
       }
     }
     .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-    .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+    .background(LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom))
     .cornerRadius(20)
     .padding(.horizontal, 20)
   }
@@ -50,7 +51,7 @@ struct FruitCardView: View {
 // MARK: - Peview
 struct FruitCardView_Previews: PreviewProvider {
   static var previews: some View {
-    FruitCardView()
+    FruitCardView(fruit: fruitsData[4])
       .previewLayout(.fixed(width: 320, height: 640))
   }
 }
