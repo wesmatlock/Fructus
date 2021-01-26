649c7925eddeafda850aabbf3cab77bfba82b9d1
wesley matlock <wesley.matlock@mlb.com>
2021-01-26 08:23:29 -0700

    SwiftUI Life Cylce.

--- Fructus/View/StartButtonView.swift
+++ Fructus/View/StartButtonView.swift
@@ -3,11 +3,12 @@ import SwiftUI
 struct StartButtonView: View {
 
   // MARK: - Properties
+  @AppStorage("isOnboarding") var isOnboading: Bool?
 
   // MARK: - Body
     var body: some View {
       Button(action: {
-        print("Exit the onboarding")
+        isOnboading = false
       }) {
         HStack(spacing: 8) {
           Text("Start")
